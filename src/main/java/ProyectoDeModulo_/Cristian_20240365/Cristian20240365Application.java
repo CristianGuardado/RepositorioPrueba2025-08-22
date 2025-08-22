3760890d842628ae2eb8af17a163e6a63c18a115
CristianGuardado <20240365@ricaldone.edu.sv>
2025-08-22 10:04:55 -0600

    hola

---
+++ src/main/java/ProyectoDeModulo_/Cristian_20240365/Cristian20240365Application.java
@@ -0,0 +1,13 @@
+package ProyectoDeModulo_.Cristian_20240365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cristian20240365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cristian20240365Application.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoDeModulo_/Cristian_20240365/Cristian20240365ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoDeModulo_.Cristian_20240365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cristian20240365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
